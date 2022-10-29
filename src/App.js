37d0d84a6838628bb449dc12119ac3924f55f5e7
Arindam Halder <halderarindam10000@gmail.com>
2022-10-29 18:13:55 +0530

    :sparkles: Trailer Added
    view trailer with fullscreen added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import Navbar from "./components/NavBar/Navbar";
 import "./App.css";
 import Movies from "./components/Movies/Movies";
+import Modal from "./components/Modal/Modal";
+import { useContext } from "react";
+import MovieContext from "./store/MovieContext";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 export default function App(){
+  const movieCtx = useContext(MovieContext);
 
   return (
     <>
       <Navbar />
       <Movies/>
+      {movieCtx.visibleTrailer && <Modal onClose={movieCtx.hideTrailer} trailer_key={movieCtx.trailer_key} visibleTrailer={movieCtx.visibleTrailer}/>}
+
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+        />
     </>
   );
 }
---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,31 @@
+import ReactDOM from "react-dom";
+import "./Modal.css";
+import { useContext } from "react";
+import MovieContext from "../../store/MovieContext";
+
+function Backdrop(props) {
+  return <div className="backdrop" onClick={props.onClose} />;
+}
+
+function ModalOverlay(props) {
+  const movieCtx = useContext(MovieContext);
+
+  return (
+    <div className="modal-container">
+      {movieCtx.visibleTrailer && 
+      <iframe src={`https://www.youtube.com/embed/${movieCtx.trailer_key}`} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+      }</div>
+  );
+}
+
+export default function Modal(props) {
+  return (
+    <>
+      {ReactDOM.createPortal(<Backdrop onClose={props.onClose}/>, document.getElementById("backdrop"))}
+      {ReactDOM.createPortal(
+        <ModalOverlay onClose={props.onClose}/>,
+        document.getElementById("overlay")
+      )}
+    </>
+  );
+}
