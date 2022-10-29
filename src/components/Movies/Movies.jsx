a61a5691548980c552dc0926a93d949091a65f2e
Arindam Halder <halderarindam10000@gmail.com>
2022-10-29 14:01:34 +0530

    :sparkles: View in Full Screen
    view movie in fullscreen

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,23 +1,78 @@
-import { useContext } from "react";
+import { useState, useContext } from "react";
 import MovieContext from "../../store/MovieContext";
 import "./Movies.css";
 import Movie from "../Movie/Movie";
+import {MdFavorite} from "react-icons/md";
+import {BsPlayFill} from "react-icons/bs";
+import { IoIosRemoveCircleOutline } from "react-icons/io"
 
 export default function Movies(){
     const movieCtx = useContext(MovieContext);
+
+    // const [isOpened, setIsOpened] = useState(false);
+    const [fullMovie, setFullMovie] = useState({}); // the movie that is going to be displayed in fullscreen mode
+
+    const IMG_PATH = "https://image.tmdb.org/t/p/w500/";
+    const NO_IMAGE_PATH = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0mPJIxvYiTXwFhUEUFkvHrM3ZT9ywgh32v7pUwKNQYguUXUdHXczKjLgr_HxHv8kcANQ&usqp=CAU";
+    const options = {year: 'numeric', month: 'short', day: 'numeric' };
+
+    function openImage(movieData){
+        movieCtx.displayFullscreen();
+        setFullMovie(movieData);
+    }
+    function addFavourite(){
+        return movieCtx.addFavouriteMovie(fullMovie);
+    }
+    function removeFavourite(){
+        return movieCtx.removeFavouriteMovie(fullMovie);
+    }
+    function checkFavourite(){
+        if(JSON.stringify(movieCtx.favouriteMovies).includes(JSON.stringify(fullMovie))){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
     return (
         <>
-            <h2 className="heading">{movieCtx.heading}</h2>
-            <div className="movies-container">
-                {movieCtx.heading !== "Favourites"?
-                    movieCtx.movies.length > 0 && movieCtx.movies.map((movie)=>{
-                        return <Movie movieData={movie} key={movie.id}/>;
-                    }):
-                    movieCtx.favouriteMovies.length>0 && movieCtx.favouriteMovies.map((favMovie)=>{
-                        return <Movie movieData={favMovie} key={favMovie.id}/>
-                    })
-                }
-            </div>
+            {!movieCtx.isOpened && 
+            <div className="view-all">
+                <h2 className="heading">{movieCtx.heading}</h2>
+                <div className="movies-container">
+                    {movieCtx.heading !== "Favourites"?
+                        movieCtx.movies.length > 0 && movieCtx.movies.map((movie)=>{
+                            return <Movie movieData={movie} key={movie.id} onImage={openImage}/>;
+                        }):
+                        movieCtx.favouriteMovies.length>0 && movieCtx.favouriteMovies.map((favMovie)=>{
+                            return <Movie movieData={favMovie} key={favMovie.id} onImage={openImage}/>
+                        })
+                    }
+                </div>
+            </div>}
+
+            {movieCtx.isOpened &&
+                    <div className="movie-fullscreen-container">
+                        <img src={fullMovie.backdrop_path ? IMG_PATH+fullMovie.backdrop_path : NO_IMAGE_PATH} alt="backdrop" className="backdropImage"/>
+                        <div className="movie-fullscreen-image-box">
+                            <img src={fullMovie.poster_path ? IMG_PATH+fullMovie.poster_path : NO_IMAGE_PATH} alt="poster" />
+                        </div>
+                        <div className="movie-fullscreen-description">
+                            <div className="movie-name">{fullMovie.title}</div>
+                            <div className="releaseDate-genre">
+                                <div className="releaseDate">{new Date(fullMovie.release_date || fullMovie.first_air_date).toLocaleDateString('en-us', options)}</div>
+                                <div className="genre">Action, Adventure, Science Fiction</div>
+                            </div>
+                            <div className="rating-fav-trailer">
+                                    <div className="rating">{fullMovie.vote_average.toFixed(1)}</div>
+                                    <div className="fav"> {checkFavourite()? <div data-title="Remove from Favourites"> <IoIosRemoveCircleOutline onClick={removeFavourite}/></div>: <div data-title="Add to Favourites"> <MdFavorite onClick={addFavourite} /></div>} </div>
+                                    <div className="trailer" data-title="Play Trailer"><BsPlayFill/> </div>
+                            </div>
+                            <h5>Overview</h5>
+                            <div className="overview"> &emsp; Suspicious that her colleague is responsible for a series of mysterious patient deaths, a nurse risks her own life to uncover the truth in this gripping thriller based on true events.</div>
+                        </div>
+                    </div>}
         </>
     );
 }
--- src/store/MovieContext.js
+++ src/store/MovieContext.js
@@ -16,18 +16,24 @@ const MovieContext = React.createContext({
     findTrendingMovies: () => {},
     addFavouriteMovie: (movieData) => {},
     removeFavouriteMovie: (movieData) => {},
+    isOpened: "",
+    displayFullscreen: ()=>{}
 });
 
+// https://api.themoviedb.org/3/tv/popular?api_key=8d9c6994c64a7b4b56743d30dcae059f&language=en-US&page=1  popular tv shows
+// https://api.themoviedb.org/3/tv/top_rated?api_key=8d9c6994c64a7b4b56743d30dcae059f&language=en-US&page=1  toprated tv shows
 export const MovieContextProvider = (props)=>{
     const [foundMovies, setFoundMovies] = useState([]);
     const [favMovies, setFavMovies] = useState(localStorage.getItem("MovieDB") || []);
     const [heading, setHeading] = useState("");
+    const [isOpened, setisOpened] = useState(false);
 
     const API_KEY = "8d9c6994c64a7b4b56743d30dcae059f";
     const findMovie = (movieName)=>{
         setHeading(`Search Result for '${movieName}'`);
+        setisOpened(false);
         const SEARCH_URL = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&page=1&include_adult=false&query=${movieName}`;
-        
+
         axios.get(SEARCH_URL).then((response)=>{
             setFoundMovies(response.data.results);
             console.log(response.data.results);
@@ -38,6 +44,7 @@ export const MovieContextProvider = (props)=>{
     }
     const findPopularMovies = ()=>{
         setHeading("Popular");
+        setisOpened(false);
         const POPULAR_URL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
         axios.get(POPULAR_URL).then((response)=>{
             setFoundMovies(response.data.results);
@@ -49,6 +56,7 @@ export const MovieContextProvider = (props)=>{
     }
     const findTopRatedMovies = ()=>{
         setHeading("Top Rated");
+        setisOpened(false);
         const TOP_RATED_URL = `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`;
         axios.get(TOP_RATED_URL).then((response)=>{
             setFoundMovies(response.data.results);
@@ -60,6 +68,7 @@ export const MovieContextProvider = (props)=>{
     }
     const findNowPlayingMovies = ()=>{
         setHeading("Now Playing");
+        setisOpened(false);
         const NOW_PLAYING_URL = `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1`;
         axios.get(NOW_PLAYING_URL).then((response)=>{
             setFoundMovies(response.data.results);
@@ -71,6 +80,7 @@ export const MovieContextProvider = (props)=>{
     }
     const findUpcomingMovies = ()=>{
         setHeading("Upcoming");
+        setisOpened(false);
         const UPCOMING_URL = `https://api.themoviedb.org/3/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1`;
         axios.get(UPCOMING_URL).then((response)=>{
             setFoundMovies(response.data.results);
@@ -82,6 +92,7 @@ export const MovieContextProvider = (props)=>{
     }
     const findTrendingMovies = ()=>{
         setHeading("Trending");
+        setisOpened(false);
         const TRENDING_URL = `https://api.themoviedb.org/3/trending/all/day?api_key=${API_KEY}`;
         axios.get(TRENDING_URL).then((response)=>{
             setFoundMovies(response.data.results);
@@ -100,6 +111,7 @@ export const MovieContextProvider = (props)=>{
     }
     const findFavouriteMovies = ()=>{   // work due
         setHeading("Favourites");
+        setisOpened(false);
         setFavMovies(JSON.parse(localStorage.getItem("MovieDB")) || []);
         return favMovies;
     }
@@ -113,6 +125,10 @@ export const MovieContextProvider = (props)=>{
         setFavMovies(JSON.parse(localStorage.getItem("MovieDB")) || []);
     },[])
 
+    const displayFullscreen = ()=>{
+        setisOpened(true);
+    }
+
     const movieContext = {
         heading: heading,
         movies: foundMovies,
@@ -125,7 +141,9 @@ export const MovieContextProvider = (props)=>{
         findUpcomingMovies: findUpcomingMovies,
         findTrendingMovies: findTrendingMovies,
         addFavouriteMovie: addFavouriteMovie,
-        removeFavouriteMovie: removeFavouriteMovie
+        removeFavouriteMovie: removeFavouriteMovie,
+        isOpened: isOpened,
+        displayFullscreen: displayFullscreen
     }
 
     return <MovieContext.Provider value={movieContext}> {props.children} </MovieContext.Provider>
